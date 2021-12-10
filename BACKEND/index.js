172d040badc6c43e26e36a7def94c825c2ec71f7
Alexandre <asilvestre786@gmail.com>
2021-12-10 19:30:14 -0300

    Edit12

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -16,31 +16,3 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 });
-
-
-app.all("/*", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    
-    next();   
-});
-
-app.get("/", (req, res) => {
-    res.send("Olá Bluemers!");
-});
-
-app.get("/filmes", (req, res) => {
-    res.send(blueFilmes);
-})
-
-app.get ("/filmes/:id", (req, res) => {
-    const idParam = req.params.id; 
-
-    const filmeEncontrado = blueFilmes.find((filme) => filme.id == idParam)
-
-    res.send(filmeEncontrado);
-});
-
-
-
-
--- FRONTEND/scripts.js
+++ FRONTEND/scripts.js
@@ -1,44 +1,29 @@
-// salvar a url do backend em uma variavel para ser ultiliza depois 
-// na hora da chamada da api
-const apiUrl = "http://localhost:3000";
+
+const apiUrl = "http://localhost:3000/filmes";
 let modoEdicao = false;
 let idEdicao = 0;
 
-// estou mapeando o elemento lista (<table></table>) do html.
 const lista = document.getElementById("lista");
 
-// crio uma funcao onde é possivel realizar uma requisicao [GET] para a api que retorna uma lista de vagas
-const getVagas = async () => {
+const getFilmes = async () => {
 
-  // FETCH - É usado para se comunicar via requisicao http (GET, POST, PUT, PATCH, DELETE);
-  // Response - é a resposta se a chamada da api foi feita com sucesso (status 200);
-  // FETCH quando nao passada configuracao e apenas a url ele faz uma chamada do tipo [GET];
-  // const chamadaApi = fetch(`${apiUrl}/vagas`)
-  // chamadaApi.then((response) => {
-  //     console.log('RESPOSTA REQUISICAO', response);
-  //     return response.json();
-  // })
-  // .then((vagas) => {
-  //     console.log('RESPOSTA DADOS JSON', vagas);
-  // })
-  const response = await fetch(`${apiUrl}/vagas`);
-  const vagas = await response.json();
+  const response = await fetch(`${apiUrl}/get-filmes`);
+  const filmes = await response.json();
   
-  // itera a lista e para cada objeto ele pode fazer alguma coisa
-  vagas.map((vaga) => {
-    console.log(vaga.empresa);
+  filmes.map((filme) => {
+    console.log(filme.titulo);
     lista.insertAdjacentHTML(
       "beforeend",
       `
             <tr>
-                <th scope="row">${vaga.id}</th>
-                <td>${vaga.empresa}</td>
-                <td>${vaga.oportunidade}</td>
-                <td>${vaga.tipo}</td>
-                <td>${vaga.salario}</td>
+                <th scope="row">${filme.id}</th>
+                <td>${filme.titulo}</td>
+                <td>${filme.genero}</td>
+                <td>${filme.imagem}</td>
+                <td>${filme.nota}</td>
                 <td>
-                    <button class="btn btn-primary" onclick="editaVaga(${vaga.id})">Editar</button>
-                    <button class="btn btn-danger" onclick="deleteVaga(${vaga.id})">Deletar</button>
+                    <button class="btn btn-primary" onclick="editaFilme(${filme.id})">Editar</button>
+                    <button class="btn btn-danger" onclick="deleteFilme(${filme.id})">Deletar</button>
                 </td>
             </tr>
         `
@@ -46,145 +31,142 @@ const getVagas = async () => {
   });
 };
 
-getVagas();
+getFilmes();
 
-const escolherVaga = async () => {
+const escolherFilme = async () => {
 
-  // buscando o que o usuario digitou no input
-  const idDigitado = document.getElementById("idVaga").value;
-  // fazendo a chamdada para a api /vagas/{id} para pegar a vaga individual
-  const response = await fetch(`${apiUrl}/vagas/${idDigitado}`);
-  // salvo o objeto retornado pelo backend;
-  const vaga = await response.json();
 
-  // mapeando a tabela do html e inserindo uma vaga dentro
-  document.getElementById("vaga").insertAdjacentHTML(
+  const idDigitado = document.getElementById("idFilme").value;
+
+  const response = await fetch(`${apiUrl}/get-by-id/${idDigitado}`);
+ 
+  const filme = await response.json();
+
+  document.getElementById("filme").insertAdjacentHTML(
     "beforeend",
     `
         <tr>
-            <th scope="row">${vaga.id}</th>
-            <td>${vaga.empresa}</td>
-            <td>${vaga.oportunidade}</td>
-            <td>${vaga.tipo}</td>
-            <td>${vaga.salario}</td>
+            <th scope="row">${filme.id}</th>
+            <td>${filme.titulo}</td>
+            <td>${filme.genero}</td>
+            <td>${filme.imagem}</td>
+            <td>${filme.nota}</td>
         </tr>
     `
   );
 };
 
-// Ele mapeia os dados do formulario que o usuario digitou e envia o objeto criado para a sua funcao responsavel (seja edicao ou cadastro)
+
 const submitForm = async () => {
 
     // mapear os inputs com os dados que o usuario digitou idependente se é edicao ou cadastro
-    const empresa = document.getElementById('empresa').value;
-    const oportunidade = document.getElementById('oportunidade').value;
-    const tipo = document.getElementById('tipo').value;
-    const salario = document.getElementById('salario').value;
-    console.log(empresa, oportunidade, tipo, salario);
+    const titulo = document.getElementById('titulo').value;
+    const genero = document.getElementById('genero').value;
+    const imagem = document.getElementById('imagem').value;
+    const nota = document.getElementById('nota').value;
+    console.log(titulo, genero, imagem, nota);
 
     // monta o objeto para ser enviado para o backend
-    const vaga = {
-        empresa,
-        oportunidade,
-        tipo,
-        salario
+    const filme = {
+        titulo,
+        genero,
+        imagem,
+        nota
     }
-    console.log(vaga);
+    console.log(filme);
 
     // JSON Stringfy = transforma um objeto/array js em um JSON string
     if(modoEdicao) {
-        putVaga(vaga);
+        putFilme(filme);
     }else {
-        postVaga(vaga);
+        postFilme(filme);
     }
     
 }
 
 // [POST] http://localhost:3000/vagas/add - Recebe o objeto transforma em JSON e envia para a api atraves do metodo post
-const postVaga = async (vaga) => {
+const postFilme = async (filme) => {
 
-    const response = await fetch(`${apiUrl}/vagas/add`, {
+    const response = await fetch(`${apiUrl}/create`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(vaga)
+        body: JSON.stringify(filme)
     })
     const data = await response.json();
     alert(data.message);
+    
     // faz a chamada para a api com algumas configuracoes****
     lista.innerHTML = '';
-    getVagas();
+    getFilmes();
     limpaCampos();
 }
 
 // [PUT] http://localhost:3000/vagas/edit/{id} - recebe o objeto transforma em json e envia para a api juntamente com o seu id para que possa
 // ser editado
-const putVaga = async (vaga) => {
+const putFilme = async (filme) => {
 
-    const response = await fetch(`${apiUrl}/vagas/edit/${idEdicao}`, {
+    const response = await fetch(`${apiUrl}/update/${idEdicao}`, {
         method: 'PUT',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(vaga)
+        body: JSON.stringify(filme)
     })
     const data = await response.json();
     alert(data.message);
-    // faz a chamada para a api com algumas configuracoes****
+
+ 
     lista.innerHTML = '';
-    getVagas();
+    getFilmes();
     limpaCampos();
 
     modoEdicao = false;
     idEdicao = 0;
 }
 
-// preenche os dados do formulario de acordo com a vaga encontrada no backend pelo seu id
-const editaVaga = async (id) => {
+const editaFilme = async (id) => {
 
     modoEdicao = true;
     idEdicao = id;
 
-    // iremos receber o id e atraves do id fazer uma chamdada para a api para buscar os dados de uma vaga por id
-    const vaga = await getById(id);
+    
+    const filme = await getById(id);
 
-    // iremos popular os inputs com os valores recebidos da chamada
-    document.getElementById('empresa').value = vaga.empresa;
-    document.getElementById('oportunidade').value  = vaga.oportunidade;
-    document.getElementById('tipo').value = vaga.tipo;
-    document.getElementById('salario').value = vaga.salario;
+    document.getElementById('titulo').value = filme.titulo;
+    document.getElementById('genero').value  = filme.genero;
+    document.getElementById('imagem').value = filme.imagem;
+    document.getElementById('nota').value = filme.nota;
 
 }
 
 
-// recebe um id e faz a chamada para a api e retorna o objeto encontrado
 const getById = async (id) => {
 
-    const response = await fetch(`${apiUrl}/vagas/${id}`)
-    const vaga = await response.json();
-    return vaga
+    const response = await fetch(`${apiUrl}/get-by-id/${id}`)
+    const filme = await response.json();
+    return filme
 }
 
-//[DELETE] http://localhost:3000/vagas/delete/1 Recebo um id e excluo a vaga do backend
-const deleteVaga = async (id) => {
-    const response = await fetch(`${apiUrl}/vagas/delete/${id}`, {
+
+const deleteFilme = async (id) => {
+    const response = await fetch(`${apiUrl}/delete/${id}`, {
         method: 'DELETE'
     })
     const result = await response.json();
     alert(result.message);
     
-    // limpos a lista de vagas para que possa ser renderizada novamente sem a vaga que excluimos
-    lista.innerHTML = '';
-    getVagas();
+        lista.innerHTML = '';
+    getFilmes();
 }
 
-// limpa os campos do formulario (inputs)
+
 const limpaCampos = () => {
 
-    document.getElementById('empresa').value = '';
-    document.getElementById('oportunidade').value = '';
-    document.getElementById('tipo').value = '';
-    document.getElementById('salario').value = '';
+    document.getElementById('titulo').value = '';
+    document.getElementById('genero').value = '';
+    document.getElementById('imagem').value = '';
+    document.getElementById('nota').value = '';
 }
 
